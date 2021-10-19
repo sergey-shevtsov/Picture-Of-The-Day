78740da5b7eacdbf637dea2332e890d9b8b1f522
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-10-20 01:49:53 +0300

    Добавил дата класс ответа с сервера

---
+++ app/src/main/java/com/sshevtsov/pictureoftheday/ui/main/PODServerResponseData.kt
@@ -0,0 +1,11 @@
+package com.sshevtsov.pictureoftheday.ui.main
+
+import com.google.gson.annotations.SerializedName
+
+data class PODServerResponseData(
+    @SerializedName("date") val date: String?,
+    @SerializedName("explanation") val explanation: String?,
+    @SerializedName("title") val title: String?,
+    @SerializedName("url") val url: String?,
+    @SerializedName("hdurl") val hdurl: String?
+)
