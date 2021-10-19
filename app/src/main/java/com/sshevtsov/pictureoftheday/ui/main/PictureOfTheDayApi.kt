b4328ec77d96a515af6778fa5e6ed9519dafa1be
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-10-20 02:07:25 +0300

    Добавил классы для запросов через Retrofit

---
+++ app/src/main/java/com/sshevtsov/pictureoftheday/ui/main/PictureOfTheDayApi.kt
@@ -0,0 +1,15 @@
+package com.sshevtsov.pictureoftheday.ui.main
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PictureOfTheDayApi {
+
+    @GET("planetary/apod")
+    fun getPictureOfTheDay(
+        @Query("api_key") apiKey: String,
+        @Query("date") date: String
+    ): Call<PODServerResponseData>
+
+}
