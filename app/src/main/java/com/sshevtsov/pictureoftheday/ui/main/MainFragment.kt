291e061f37e41c8e9d36213bc65e563876f91a72
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-10-28 15:52:22 +0300

    Добавил tab layout и присоединил его к view pager 2

--- app/src/main/java/com/sshevtsov/pictureoftheday/ui/main/MainFragment.kt
+++ app/src/main/java/com/sshevtsov/pictureoftheday/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
+import com.sshevtsov.pictureoftheday.R
 import com.sshevtsov.pictureoftheday.databinding.MainFragmentBinding
 import com.sshevtsov.pictureoftheday.ui.main.api.PODViewPagerAdapter
 
@@ -31,7 +32,25 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initViewPager()
+    }
+
+    private fun initViewPager() {
         binding.viewPager.adapter = PODViewPagerAdapter(requireActivity())
         binding.viewPager.currentItem = 2
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = getString(R.string.before_yesterday)
+                }
+                1 -> {
+                    tab.text = getString(R.string.yesterday)
+                }
+                2 -> {
+                    tab.text = getString(R.string.today)
+                }
+            }
+        }.attach()
     }
 }
