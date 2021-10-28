971dba801e2b126a902306440fd5fcafe973e2d8
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-10-28 16:48:20 +0300

    Добавил обработку нажатий на пункты навигации

--- app/src/main/java/com/sshevtsov/pictureoftheday/ui/MainActivity.kt
+++ app/src/main/java/com/sshevtsov/pictureoftheday/ui/MainActivity.kt
@@ -4,21 +4,56 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.sshevtsov.pictureoftheday.R
+import com.sshevtsov.pictureoftheday.databinding.ActivityMainBinding
 import com.sshevtsov.pictureoftheday.ui.main.MainFragment
+import com.sshevtsov.pictureoftheday.ui.settings.SettingsFragment
 import com.sshevtsov.pictureoftheday.util.isDarkMode
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         setSavedTheme()
+        initBottomNavigation()
 
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commit()
+        }
+    }
+
+    private fun initBottomNavigation() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_one -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.container, MainFragment.newInstance())
+                        .commit()
+                    true
+                }
+                R.id.page_two -> {
+                    true
+                }
+                R.id.page_three -> {
+                    true
+                }
+                R.id.settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, SettingsFragment.newInstance())
+                        .addToBackStack(null)
+                        .commit()
+                    true
+                }
+                else -> false
+            }
         }
     }
 
